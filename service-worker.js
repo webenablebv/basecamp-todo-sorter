02ecc0c8a9fb9b207606653d1ff4de21c9ae775e
Henk Mollema <henk@webenable.nl>
2025-12-22 09:29:31 +0100

    Use wildcard for accoubnt ID

--- service-worker.js
+++ service-worker.js
@@ -4,10 +4,10 @@ let currentUrl = '';
 chrome.webNavigation.onHistoryStateUpdated.addListener(async details => {
   tabId = details.tabId;
   currentUrl = details.url;
-}, { urls: ['https://3.basecamp.com/4470011/buckets/*'] });
+}, { urls: ['https://3.basecamp.com/*/buckets/*'] });
 
 chrome.webRequest.onCompleted.addListener(details => {
   if (currentUrl != null && tabId > 0) {
     chrome.tabs.sendMessage(tabId, { type: 'page-rendered' });
   }
-}, { urls: ['https://3.basecamp.com/4470011/buckets/*'] });
+}, { urls: ['https://3.basecamp.com/*/buckets/*'] });
